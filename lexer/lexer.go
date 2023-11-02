96e5b692a3d1a89950642303191d8c8b82051967
galexw <galexwong.github@gmail.com>
2023-11-02 18:11:50 -0400

    Adding single length tokenization in lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -11,12 +11,60 @@ type Lexer struct {
 
 func New(input string) *Lexer { // Input here is actually the source code in Monkey
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token { // ch is the character that is being read
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
 
 func (l *Lexer) NextToken() token.Token {
 	// TODO: Implement next token method
-	return token.Token{}
+	var tok token.Token
+
+	switch l.ch {
+	// Operators
+	case '=':
+		tok = newToken(token.Assign, l.ch)
+	case '+':
+		tok = newToken(token.Plus, l.ch)
+	case '-':
+		tok = newToken(token.Minus, l.ch)
+	case '!':
+		tok = newToken(token.Bang, l.ch)
+	case '*':
+		tok = newToken(token.Asterisk, l.ch)
+	case '/':
+		tok = newToken(token.Slash, l.ch)
+	case '<':
+		tok = newToken(token.LessThan, l.ch)
+	case '>':
+		tok = newToken(token.GreaterThan, l.ch)
+
+	// Delimiters
+	case ';':
+		tok = newToken(token.Semicolon, l.ch)
+	case '(':
+		tok = newToken(token.LeftParen, l.ch)
+	case ')':
+		tok = newToken(token.RightParen, l.ch)
+	case ',':
+		tok = newToken(token.Comma, l.ch)
+	case '{':
+		tok = newToken(token.LeftBrace, l.ch)
+	case '}':
+		tok = newToken(token.RightBrace, l.ch)
+	case '[':
+		tok = newToken(token.LeftBracket, l.ch)
+	case ']':
+		tok = newToken(token.RightBracket, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.readChar()
+	return tok
 }
 
 func (l *Lexer) readChar() {
