47aa163be9df2590b0f9556c3ec4529dd132c222
Galex Wong <galexwong@meta.com>
2023-12-31 17:30:46 -0500

    Added function literal evaluation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -71,6 +71,11 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
+
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		block := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: block}
 	}
 
 	return nil
